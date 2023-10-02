fc48a6001d161bcc7c372da642177ace92eca037
radherathod <65850061+radherathod@users.noreply.github.com>
2023-10-02 15:48:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [value, setValue] = useState("");
+  return (
+    <div className="App">
+      <div className="calculator">
+        <form action="">
+          <div>
+            <input type="text" value={value} />
+          </div>
+
+          <div>
+            <input type="button" value="AC" onClick={(e) => setValue("")} />
+            <input
+              type="button"
+              value="DC"
+              onClick={(e) => setValue(value.slice(0, -1))}
+            />
+            <input
+              type="button"
+              value="."
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="/"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+          </div>
+
+          <div>
+            <input
+              type="button"
+              value="7"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="8"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="9"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="*"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+          </div>
+
+          <div>
+            <input
+              type="button"
+              value="4"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="5"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="6"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="+"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+          </div>
+
+          <div>
+            <input
+              type="button"
+              value="1"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="2"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="3"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="-"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+          </div>
+
+          <div>
+            <input
+              type="button"
+              value="00"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="0"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="="
+              onClick={(e) => setValue(eval(value))}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
